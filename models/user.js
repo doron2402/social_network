6c434e74f3a5d6049dc5b911e4f05afe571fb77d
Doron Segal <doron2402@gmail.com>
2014-05-31 20:16:59 -0700

    adding user models

---
+++ models/user.js
@@ -0,0 +1,27 @@
+var User = {};
+
+//Return Bool
+User.isFollow = function(user_id, target_id, cb){
+
+};
+
+//return a number
+User.getFollowers = function(user_id, cb) {
+
+};
+
+User.followTargetUser = function(user_id, target_id, cb) {
+
+};
+
+User.unFollowTargetUser = function(user_id, target_id, cb) {
+    User.isFollow(user_id, target_id, function(err, result){
+        if (err) {
+            cb(err, null);
+        }
+        if (!result){
+            cb(SN.Errors.ResourceNotFound,null);
+        }
+        
+    });
+}
